1cbba94963e27ef6f33a1373ab843e532b65b976
Linus Tran <linh@Linuss-MacBook-Pro.local>
2025-12-15 17:10:53 +0700

    feat: remove unnecessary code

--- web/src/pages/Index.tsx
+++ web/src/pages/Index.tsx
@@ -4,38 +4,11 @@ import { motion } from "framer-motion";
 import { LoginForm } from "@/components/auth/LoginForm";
 import { RegisterForm } from "@/components/auth/RegisterForm";
 import { User } from "@/types/auth";
-import { OpenRouter } from "@openrouter/sdk";
-const openRouter = new OpenRouter({
-  apiKey: "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",
-  // defaultHeaders: {
-  //   "HTTP-Referer": "http://localhost:8080/", // Optional. Site URL for rankings on openrouter.ai.
-  //   "X-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
-  // },
-});
+
 const Index = () => {
   const navigate = useNavigate();
   const [isLogin, setIsLogin] = useState(true);
   const [user, setUser] = useState<User | null>(null);
-
-  const getCompletion = async () => {
-    const completion = await openRouter.chat.send({
-      model: "openai/gpt-oss-20b:free",
-      messages: [
-        {
-          role: "user",
-          content: "give me three sentences about the meaning of life?",
-        },
-      ],
-      stream: true,
-    });
-    for await (const chunk of completion) {
-      document.getElementById("completion")?.append(chunk.choices[0].delta.content);
-      sleep(50);
-    }
-    function sleep(ms: number) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-  };
 
   useEffect(() => {
     // Check if user is already logged in
@@ -70,8 +43,6 @@ const Index = () => {
           <RegisterForm onSwitchToLogin={() => setIsLogin(true)} />
         )}
       </motion.div>
-      <button onClick={getCompletion}>Get Completion</button>
-      <div id="completion"></div>
     </div>
   );
 };
